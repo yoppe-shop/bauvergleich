d02884d3a67ec44b09e5efc494d8b86edba6e672
stefa <stefa@DESKTOP-OTL5EGK.local>
2018-11-02 08:25:31 +0100

    Added state and condition in rendering

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {connect} from 'react-redux';
 import {PropTypes} from 'prop-types';
 
 class App extends Component {
@@ -9,13 +10,36 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isClicked: false
+        };
     }
+
+    handleClick = e => {
+        e.preventDefault();
+        this.setState({
+            isClicked: true
+        });
+    };
 
     render() {
         const {test} = this.props;
-        return <div>Hallo, wie gehts euch? {test}</div>
+        const {isClicked} = this.state;
+
+        return (
+            <div>
+                <div>Hallo, wie gehts euch? {test}</div>
+                <a href="#" onClick={this.handleClick}>
+                Click me
+                </a>
+                <div>
+                { isClicked &&
+                    <div>Es wurde geklickt!</div>
+                }
+                </div>
+            </div>
+        );
+
     }
 }
-
 export default App;
